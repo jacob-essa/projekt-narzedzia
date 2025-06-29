96ca1f92ae0db1c5911db23ce8f1b7a97d9990b9
jakubcwiora <jakubcwiora@wp.pl>
2025-06-30 00:44:02 +0200

    Starting Express

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const { createServer } = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const hostname = "localhost"
+const port = 4200
+
+// Add this line to serve static files
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/static/template.html'));
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Listening on port ${port}`);
+});
